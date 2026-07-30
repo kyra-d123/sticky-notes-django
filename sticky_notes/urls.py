c540f0ecdd69fa63759d85cbc15c125cf02c1edd
kyra-d123 <kyra@daines.co.za>
2026-07-30 16:32:25 +0200

    Create urls.py

---
+++ sticky_notes/urls.py
@@ -0,0 +1,9 @@
+"""Define the root URL configuration for the project."""
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("notes.urls")),
+]
